c2a3bf6b204f1d8da670d12a99eee405fd213e5f
kda <kda007@skbkontur.ru>
2016-12-20 15:25:38 +0700

    LOG_CONTEXT

--- liblog/include/liblog/liblog.h
+++ liblog/include/liblog/liblog.h
@@ -13,6 +13,14 @@
 #define LOG_DEBUG LOG_IF(liblog::LogLevel::LOG_LEVEL_DEBUG, liblog::LOGGER != nullptr && liblog::LOGGER->check_log_level(liblog::LogLevel::LOG_LEVEL_DEBUG))
 #define LOG_ERROR LOG_IF(liblog::LogLevel::LOG_LEVEL_ERROR, liblog::LOGGER != nullptr && liblog::LOGGER->check_log_level(liblog::LogLevel::LOG_LEVEL_ERROR))
 
+#define LOG_CONTEXT(level, context) liblog::LogMessage(liblog::LOGGER.get(), context, level)
+#define LOG_IF_CONTEXT(level, condition, context) !(condition) ? (void)0 : LOG(level, context)
+
+#define LOG_WARN_CONTEXT(context) LOG_IF_CONTEXT(liblog::LogLevel::LOG_LEVEL_WARN, liblog::LOGGER != nullptr && liblog::LOGGER->check_log_level(liblog::LogLevel::LOG_LEVEL_WARN), context)
+#define LOG_INFO_CONTEXT(context) LOG_IF_CONTEXT(liblog::LogLevel::LOG_LEVEL_INFO, liblog::LOGGER != nullptr && liblog::LOGGER->check_log_level(liblog::LogLevel::LOG_LEVEL_INFO), context)
+#define LOG_DEBUG_CONTEXT(context) LOG_IF_CONTEXT(liblog::LogLevel::LOG_LEVEL_DEBUG, liblog::LOGGER != nullptr && liblog::LOGGER->check_log_level(liblog::LogLevel::LOG_LEVEL_DEBUG), context)
+#define LOG_ERROR_CONTEXT(context) LOG_IF_CONTEXT(liblog::LogLevel::LOG_LEVEL_ERROR, liblog::LOGGER != nullptr && liblog::LOGGER->check_log_level(liblog::LogLevel::LOG_LEVEL_ERROR), context)
+
 namespace liblog
 {
 	void set_logger(std::unique_ptr<ILogger>&& logger);
